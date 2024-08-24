f40a20e52fb9253ea8e17683adcb294cea2b3757
Byron Blank <byronbla@gmail.com>
2024-08-24 12:08:33 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NeopixelControlDesktop;
+
+var res = await AdafruitIOService.SendValue("a");
+
+Console.Write(res);
+
+Console.WriteLine("Hello, World!");
+Console.Read();
